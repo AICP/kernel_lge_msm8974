8dc0beb2c3bc5f8904d1a35994955ec9a45e6236
Paul Reioux <reioux@gmail.com>
2013-11-02 19:11:54 -0500

    Powersuspend
    kernel/power/powersuspend: default to userspace for now
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    kernel/power/powersuspend: add back userpace control w/ default kernel control
    
    make kernel / userspace mode switchable
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    kernel/power/powersuspend: remove userspace dependency from powersuspend
    
    make powersuspend not depend on a userspace initiator anymore, but use a hook
    in autosleep instead.
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    powersuspend: new PM kernel driver for Android w/o early_suspend
    
    Android early_suspend/late_resume PM kernel driver framework has been
    deprecated by Google. This new powersuspend PM kernel driver is a replacement
    for it and existing early_suspend drivers can be easily adapted to use this
    new replacement driver
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    powersuspend: fix logci derps :p
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    kernel/power/powersuspend: LCD screen on/off hooks (Yank555.lu)
    - add an alternative hook for screen on / off detection in the display
      pannel driver
    - sleeps  ~0.1s sooner, wakes up ~1s later then autosleep
    - clean up source formatting a bit (Paul Reioux)
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    	kernel/power/powersuspend.c
    
    kernel/power/powersuspend: cumulative update to version 1.5
    - fix hybrid-kernel mode cannot be set through sysfs
    
    kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend v1.4 (Yank555.lu)
    
    - add hybrid-kernel mode (autosleep and panel, first wins)
    - default to hybrid-kernel mode
    - harmonize debug message with my other stuff
    - include all 3 modes as default, so it's only commenting out to change
    
    kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend v1.3 (Yank555.lu)
    
    - fix stupid typo
    - add an alternative hook for screen on / off detection in the display panel driver
    (sleeps ~0.1s sooner, wakes up ~1s later then autosleep)
    
    kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend v1.2 (Yank555.lu)
    
    - make kernel / userspace mode switchable
    
    kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend v1.1 (Yank555.lu)
    
    - make powersuspend not depend on a userspace initiator anymore, but use a hook in autosleep instead.
    
    kernel/power/powersuspend: new PM kernel driver for Android w/o early_suspend (faux123)
    
    Android early_suspend/late_resume PM kernel driver framework has been
    deprecated by Google. This new powersuspend PM kernel driver is a replacement
    for it and existing early_suspend drivers can be easily adapted to use this
    new replacement driver
    
    Change-Id: I4b5c27f6903fa61c9f87f2a4573b2a728b4e2655
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- kernel/power/autosleep.c
+++ kernel/power/autosleep.c
@@ -11,6 +11,10 @@
 #include <linux/pm_wakeup.h>
 #include <linux/zwait.h>
 
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
+
 #include "power.h"
 
 static suspend_state_t autosleep_state;
@@ -106,8 +110,16 @@ int pm_autosleep_set_state(suspend_state_t state)
 		pm_wakep_autosleep_enabled(true);
 		zw_queue_up_suspend_work(state);
 		queue_up_suspend_work();
+#ifdef CONFIG_POWERSUSPEND
+		// Yank555.lu : add hook to handle powersuspend tasks (sleep)
+		set_power_suspend_state_autosleep_hook(POWER_SUSPEND_ACTIVE);
+#endif
 	} else {
 		pm_wakep_autosleep_enabled(false);
+#ifdef CONFIG_POWERSUSPEND
+		// Yank555.lu : add hook to handle powersuspend tasks (wakeup)
+		set_power_suspend_state_autosleep_hook(POWER_SUSPEND_INACTIVE);
+#endif
 	}
 
 	mutex_unlock(&autosleep_lock);
