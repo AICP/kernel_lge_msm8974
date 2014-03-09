600b9b6f7e39a25d80628afd916af4955a7a635d
Snuzzo <jrracinfan@gmail.com>
2014-03-09 15:34:02 -0400

    Added DanceDance CPU Governor
    Change-Id: I41436e8c7fa8aa68ea92645f3527dc74ed2a8afb

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -388,7 +388,9 @@ extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
-
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
+extern struct cpufreq_governor cpufreq_gov_dancedance;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_dancedance)
 #endif
 
 
