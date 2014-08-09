3046fb8ead6718ee3dbbfbfe5572edb66bf90304
Drgravy <drg113001@gmail.com>
2014-08-09 17:41:08 -0500

    Revert "cpufreq: interactive: fix derp."
    This reverts commit 6302a52874567d1ce357c3ffc55bdb6077cdadc0.
    
    Change-Id: Ie7e2bb93f43fbe967f561a6652caade08dd57d02

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -459,8 +459,7 @@ static void cpufreq_interactive_timer(unsigned long data)
 			new_freq = choose_freq(pcpu, loadadjfreq);
 
 			if (new_freq > boosted_freq &&
-					pcpu->target_freq < hispeed_freq)
-				new_freq = boosted_freq;
+				pcpu->target_freq < hispeed_freq);
 		}
 	}
 	else
