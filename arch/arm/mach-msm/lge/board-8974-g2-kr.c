34f235cfff6b407a66214c1d37e9c3b8415e5134
Snuzzo <jrracinfan@gmail.com>
2014-09-07 10:51:28 -0400

    Updated thermal config
    Change-Id: Id4fb1f1a9e115e314448981d0aa27dbd652a1d8d

--- arch/arm/mach-msm/lge/board-8974-g2-kr.c
+++ arch/arm/mach-msm/lge/board-8974-g2-kr.c
@@ -102,18 +102,18 @@ static void __init msm8974_early_memory(void)
 static struct msm_thermal_data msm_thermal_pdata = {
 	.sensor_id = 0,
 	.poll_ms = 400,
-	.shutdown_temp = 110,
+	.shutdown_temp = 90,
 
 	.allowed_max_high = 85,
-	.allowed_max_low = 81,
+	.allowed_max_low = 82,
 	.allowed_max_freq = 300000,
 
-	.allowed_mid_high = 82,
-	.allowed_mid_low = 77,
+	.allowed_mid_high = 81,
+	.allowed_mid_low = 78,
 	.allowed_mid_freq = 960000,
 
-	.allowed_low_high = 80,
-	.allowed_low_low = 75,
+	.allowed_low_high = 77,
+	.allowed_low_low = 65,
 	.allowed_low_freq = 1497600,
 };
 #endif
